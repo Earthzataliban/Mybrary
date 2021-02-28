ae2fd58a264d8eba0fbdb6e4194122722d904c64
Earthzataliban <milereshawn@gmail.com>
2021-03-01 06:18:50 +0700

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRounter = require('./routes/index');
+const authorRounter = require('./routes/authors');
 
-app.set('view engine','ejs');
-app.set('views',__dirname + '/views');
-app.set('layout', 'layouts/layout');
-app.use(expressLayouts);
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose');
 const { use } = require('./routes/index');
@@ -25,5 +28,6 @@ db.on('error',error => console.error(error));
 db.once('open',() => console.log('Connected to Mongoose'));
 
 app.use('/', indexRounter);
+app.use('/authors', authorRounter);
 
 app.listen(process.env.PORT ||3000);
